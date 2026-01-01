74a535df0f7c796d4856dc9441039e48af01e126
AkhilSK7 <akhilskumar1599@gmail.com>
2026-01-01 22:46:58 +0530

    Implement resume text extraction from uploaded PDF and DOCX files

--- backend/resumeanalyzer/views.py
+++ backend/resumeanalyzer/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.template.context_processors import request
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
 from resumeanalyzer.models import Resume
 from resumeanalyzer.serializers import ResumeSerializer
 from rest_framework.permissions import IsAuthenticated
+from docx import Document
+from rest_framework.views import APIView
+from pypdf import PdfReader
+import os
 # Create your views here.
 
 class ResumeAPIView(ModelViewSet):
@@ -12,4 +17,28 @@ class ResumeAPIView(ModelViewSet):
     def get_queryset(self):
         return Resume.objects.filter(user=self.request.user)
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        resume=serializer.save(user=self.request.user)
+        extracted_text = ""
+        try:
+            path=resume.file.path
+            extension=os.path.splitext(path)[1].lower()
+            if extension=='.pdf':
+                reader=PdfReader(resume.file.path)
+                for page in reader.pages:
+                    extracted_text+=page.extract_text() or ""
+            elif extension=='.docx':
+                doc=Document(resume.file.path)
+                for para in doc.paragraphs:
+                    extracted_text+=para.text+'\n'
+            else:
+                raise ValidationError({"file": "Unsupported file type. Please upload PDF or DOCX."})
+        except ValidationError:
+            raise
+        except Exception:
+            raise ValidationError({"file": "Failed to read the uploaded file."})
+
+        resume.extracted_text=extracted_text.strip()
+        resume.save()
+
+
+
